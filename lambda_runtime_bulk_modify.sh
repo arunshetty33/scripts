2e1d74ddf8e563a81a785671ac2dc4ee34244f9a
Arun Shetty <arun.shetty@happiestminds.com>
2019-04-12 11:15:19 +0530

    Update lambda_runtime_bulk_modify.sh

--- lambda_runtime_bulk_modify.sh
+++ lambda_runtime_bulk_modify.sh
@@ -4,9 +4,9 @@
 region="us-east-1"
 check_runtime="nodejs8.10"
 upgrade_runtime="nodejs8.10"
-aws_profile="fams-sandbox"
+aws_profile="aws-profile"
 file='lambda_function_list.txt'
-aws lambda list-functions --profile fams-sandbox --region us-east-1 --query 'Functions[].[FunctionName,Runtime]' --output text | grep $check_runtime | awk '{print $1}' 2>&1 | tee $file
+aws lambda list-functions --profile $aws_profile --region $region --query 'Functions[].[FunctionName,Runtime]' --output text | grep $check_runtime | awk '{print $1}' 2>&1 | tee $file
 #upgrade or downgrade
 for i in `cat $file`
 do
